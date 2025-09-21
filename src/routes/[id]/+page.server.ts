38c3ff32f878f0b6262aa0bbf22c840394d71e8a
Abinash Sahoo <implabinash@gmail.com>
2025-09-21 22:17:42 +0530

    fix: add any youtube url support

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -3,7 +3,17 @@ import type { Actions } from "./$types";
 export const actions = {
 	youtube: async ({ request }) => {
 		const formData = Object.fromEntries(await request.formData());
-		const videoCode = formData.videoURL.toString().split("/").at(-1);
+		let videoCode = "";
+
+		// Check for valid youtube video or not
+
+		if (formData.videoURL.toString().includes("watch?v=")) {
+			videoCode = formData.videoURL.toString().split("watch?v=").at(-1) || "";
+		} else {
+			videoCode = formData.videoURL.toString().split("/").at(-1) || "";
+		}
+
+		// if videoCode is empty return an error
 
 		return { videoCode };
 	}
