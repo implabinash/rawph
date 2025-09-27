42990ffda0891c86a84113898146e4886477c011
Abinash Sahoo <implabinash@gmail.com>
2025-09-27 19:59:31 +0530

    feat: add request pending pop up

---
+++ src/lib/components/Popup.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Clock } from "@lucide/svelte";
+</script>
+
+<section class="fixed inset-0 grid h-full place-items-center bg-black/40 backdrop-blur-sm">
+	<div
+		class="space-y-6 rounded-lg border border-neutral-border bg-default-background p-8 shadow-lg"
+	>
+		<div class="space-y-4 text-center">
+			<img
+				src="/images/placeholders/0.png"
+				alt="Person Name"
+				class="mx-auto size-14 rounded-full"
+			/>
+
+			<div class="space-y-2">
+				<h1 class="text-heading-2">Waiting for approval</h1>
+
+				<p class="text-body text-subtext-color">
+					Alex Chen needs to approve your request to join the session.
+				</p>
+			</div>
+		</div>
+
+		<div class="mx-auto flex items-center justify-center gap-2">
+			<Clock size="20px" class="text-btext-brand-800 rounded-full bg-brand-100 p-1" />
+
+			<p class="text-caption text-subtext-color">Usually responds within a few minutes</p>
+		</div>
+
+		<form action="">
+			<button
+				class="w-full cursor-pointer rounded-md border border-neutral-border py-2 text-body-bold text-neutral-700 hover:bg-neutral-50 active:bg-default-background"
+				>Cancel Request</button
+			>
+		</form>
+	</div>
+</section>
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -5,26 +5,35 @@
 	import Controls from "$lib/components/Controls.svelte";
 	import Video from "$lib/components/Video.svelte";
 	import Chat from "$lib/components/Chat.svelte";
+	import Popup from "$lib/components/Popup.svelte";
 
 	let { form }: { form: ActionData } = $props();
+
+	let isApproved: boolean = $state(true);
 </script>
 
-<main class="flex h-screen gap-4 p-4">
-	<section class="flex h-full w-192 flex-col gap-4">
-		<div class="h-112 rounded-lg border border-neutral-border bg-neutral-50 p-4">
-			<Video {form} />
-		</div>
+<main class="relative">
+	<section class="flex h-screen gap-4 p-4">
+		<section class="flex h-full w-192 flex-col gap-4">
+			<div class="h-112 rounded-lg border border-neutral-border bg-neutral-50 p-4">
+				<Video {form} />
+			</div>
 
-		<div class="flex min-h-0 flex-1 gap-4">
-			<Controls />
+			<div class="flex min-h-0 flex-1 gap-4">
+				<Controls />
 
-			<Chat />
-		</div>
+				<Chat />
+			</div>
+		</section>
+
+		<section
+			class="relative flex h-full flex-1 flex-col gap-2 rounded-lg border border-neutral-border bg-neutral-50 p-4"
+		>
+			<Whiteboard />
+		</section>
 	</section>
 
-	<section
-		class="relative flex h-full flex-1 flex-col gap-2 rounded-lg border border-neutral-border bg-neutral-50 p-4"
-	>
-		<Whiteboard />
-	</section>
+	{#if !isApproved}
+		<Popup />
+	{/if}
 </main>
