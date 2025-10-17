6c32c441bcf79898617726dab1868bd20e9c1bcc
Abinash Sahoo <implabinash@gmail.com>
2025-10-17 12:13:04 +0530

    feat: integrate better in sing in form action

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,18 +1,52 @@
 import { z } from "zod/v4";
 
-import type { Actions } from "@sveltejs/kit";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 
 import { signInSchema } from "$lib/validations/auth";
+import { authClient } from "$lib/auth-client";
 
 export const actions = {
 	manual: async ({ request }) => {
 		const formData = Object.fromEntries(await request.formData());
 		const result = signInSchema.safeParse(formData);
 
-		console.log(result);
-
 		if (!result.success) {
-			return { success: false, data: formData.email, error: z.flattenError(result.error) };
+			return fail(400, {
+				success: false,
+				data: { email: formData.email },
+				error: z.flattenError(result.error).fieldErrors,
+				message: ""
+			});
 		}
+
+		const { date, error } = await authClient.signIn.email({
+			email: result.data.email,
+			password: result.data.password,
+			callbackURL: "/dashboard"
+		});
+
+		if (error) {
+			console.error("Better Auth sign in error:", error);
+
+			if (error.status === 409) {
+				return fail(409, {
+					data: {
+						email: result.data.email
+					},
+					errors: { email: ["An account with this email already exists"] },
+					message: error.message || "User already exists."
+				});
+			}
+
+			return fail(error.status || 400, {
+				data: {
+					email: result.data.email
+				},
+				errors: {},
+				message: error.message || "Sign in failed. Please try again."
+			});
+		}
+
+		return redirect(303, "/dashboard");
 	}
 } satisfies Actions;
