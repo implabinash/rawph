bfa392ba096eb89b52102a25eb951b8dc9131624
Abinash Sahoo <implabinash@gmail.com>
2025-10-21 21:33:41 +0530

    fix: cookie setiings

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -25,7 +25,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 							secure: true,
 							httpOnly: true,
 							sameSite: "none",
-							maxAge: 30 * 24 * 60 * 60
+							maxAge: 30 * 24 * 60 * 60,
+							partitioned: true
 						});
 					}
 
--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -72,7 +72,8 @@ export const actions = {
 				secure: true,
 				httpOnly: true,
 				sameSite: "none",
-				maxAge: 30 * 24 * 60 * 60
+				maxAge: 30 * 24 * 60 * 60,
+				partitioned: true
 			});
 		}
 
