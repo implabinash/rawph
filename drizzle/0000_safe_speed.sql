30bd9ecb27ee45fa34d856d90f28f5255e6dffe7
Abinash Sahoo <implabinash@gmail.com>
2025-10-22 23:44:34 +0530

    fix: add new migrateions

--- drizzle/0000_safe_speed.sql
+++ drizzle/0000_safe_speed.sql
@@ -10,14 +10,30 @@ CREATE TABLE `sessions` (
 --> statement-breakpoint
 CREATE UNIQUE INDEX `sessions_id_unique` ON `sessions` (`id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `sessions_token_unique` ON `sessions` (`token`);--> statement-breakpoint
+CREATE TABLE `invite_codes` (
+	`id` text PRIMARY KEY NOT NULL,
+	`code` text NOT NULL,
+	`created_by` text DEFAULT '00000000-0000-0000-0000-000000000000',
+	`is_used` integer DEFAULT false NOT NULL,
+	`used_by` text,
+	`used_at` integer,
+	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `invite_codes_id_unique` ON `invite_codes` (`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `invite_codes_code_unique` ON `invite_codes` (`code`);--> statement-breakpoint
 CREATE TABLE `users` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`password` text NOT NULL,
 	`image` text NOT NULL,
+	`is_invited` integer DEFAULT false NOT NULL,
+	`joined_code` text,
 	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
-	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL
+	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	FOREIGN KEY (`joined_code`) REFERENCES `invite_codes`(`code`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_id_unique` ON `users` (`id`);--> statement-breakpoint
--- drizzle/0001_supreme_pepper_potts.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` ADD `is_invited` integer DEFAULT false NOT NULL;
