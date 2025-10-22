685f6d9a8ede29abf6d62af88f4dc61eb9420dbc
Abinash Sahoo <implabinash@gmail.com>
2025-10-22 14:51:07 +0530

    feat: create change password endpoint

---
+++ src/routes/api/v1/auth/change-password/+server.ts
@@ -0,0 +1,96 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod/v4";
+
+import type { RequestHandler } from "./$types";
+import { json } from "@sveltejs/kit";
+
+import { getSessionData } from "$lib/db/queries/sessions.query";
+import { hashPassword, verifyPassword } from "$lib/utils/hash";
+import { changePasswordSchema } from "$lib/validations/auth";
+import { usersTable } from "$lib/db/schemas/user.schema";
+import { COOKIE_NAME } from "$lib/utils/constants";
+
+export const POST: RequestHandler = async ({ request, cookies, locals }) => {
+	const body = await request.json();
+
+	const result = changePasswordSchema.safeParse(body);
+
+	if (!result.success) {
+		const response = {
+			success: false,
+			data: {},
+			error: z.flattenError(result.error).fieldErrors,
+			message: "Invalid inputs."
+		};
+
+		return json(response, { status: 400 });
+	}
+
+	const sessionToken = cookies.get(COOKIE_NAME);
+
+	if (!sessionToken) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Invalid session."
+		};
+
+		return json(response, { status: 401 });
+	}
+
+	const data = await getSessionData(locals.db, sessionToken);
+
+	if (!data || !data.users) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Invalid session."
+		};
+
+		return json(response, { status: 401 });
+	}
+
+	const isValidPassword = await verifyPassword(result.data.currentPassword, data.users.password);
+
+	if (!isValidPassword) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Current password is incorrect."
+		};
+
+		return json(response, { status: 401 });
+	}
+
+	const newHashedPassword = await hashPassword(result.data.newPassword);
+
+	try {
+		await locals.db
+			.update(usersTable)
+			.set({ password: newHashedPassword })
+			.where(eq(usersTable.id, data.users.id));
+	} catch (err) {
+		console.error("Password change error: ", err);
+
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Password change failed. Try Again."
+		};
+
+		return json(response, { status: 500 });
+	}
+
+	const response = {
+		success: true,
+		data: {},
+		error: {},
+		message: "Password changed successfully."
+	};
+
+	return json(response, { status: 200 });
+};
