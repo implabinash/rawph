0ca91c2f467c58b74fb67698d49bde0d56144c6d
Abinash Sahoo <implabinash@gmail.com>
2025-10-22 14:30:49 +0530

    feat: create get user by session endpoint

---
+++ src/routes/api/v1/users/me/+server.ts
@@ -0,0 +1,93 @@
+import { eq } from "drizzle-orm";
+
+import type { RequestHandler } from "./$types";
+import { json } from "@sveltejs/kit";
+
+import { COOKIE_NAME, EXTENSION_THRESHOLD, SESSION_DURATION } from "$lib/utils/constants";
+import { getSessionData } from "$lib/db/queries/sessions.query";
+import { sessionsTable } from "$lib/db/schemas/auth.schema";
+
+export const GET: RequestHandler = async ({ cookies, locals }) => {
+	const sessionToken = cookies.get(COOKIE_NAME);
+
+	if (!sessionToken) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Invalid session."
+		};
+
+		return json(response, { status: 401 });
+	}
+
+	const data = await getSessionData(locals.db, sessionToken);
+
+	if (!data || !data.users) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Invalid session."
+		};
+
+		return json(response, { status: 401 });
+	}
+
+	const now = new Date();
+	const expiresAt = new Date(data.sessions.expiresAt);
+	const timeUntilExpiry = expiresAt.getTime() - now.getTime();
+
+	if (timeUntilExpiry <= 0) {
+		const response = {
+			success: false,
+			data: {},
+			error: {},
+			message: "Session expired. Please log in again."
+		};
+		return json(response, { status: 401 });
+	}
+
+	if (timeUntilExpiry < EXTENSION_THRESHOLD) {
+		const newExpiresAt = new Date(now.getTime() + SESSION_DURATION);
+
+		try {
+			await locals.db
+				.update(sessionsTable)
+				.set({ expiresAt: newExpiresAt })
+				.where(eq(sessionsTable.token, sessionToken));
+		} catch (err) {
+			console.error("Failed to extend session:", err);
+
+			const response = {
+				success: false,
+				data: {},
+				error: {},
+				message: "Failed to retrieve user data. Try again."
+			};
+
+			return json(response, { status: 500 });
+		}
+
+		cookies.set(COOKIE_NAME, sessionToken, {
+			path: "/",
+			secure: true,
+			httpOnly: true,
+			sameSite: "lax",
+			expires: newExpiresAt
+		});
+	}
+
+	const { password, ...userWithoutPassword } = data.users;
+
+	const response = {
+		success: true,
+		data: {
+			user: userWithoutPassword
+		},
+		error: {},
+		message: "User retrieved successfully."
+	};
+
+	return json(response, { status: 200 });
+};
