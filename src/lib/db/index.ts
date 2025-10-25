bb7c7216f74a0888acf2521d85cae13270ab245f
Abinash Sahoo <implabinash@gmail.com>
2025-10-25 18:58:44 +0530

    fix: add study session tables to instances

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,16 +1,26 @@
 import { drizzle } from "drizzle-orm/d1";
 
-import { inviteCodesTable } from "./schemas/invite.schema";
 import { authSessionsTable, oauthAccountsTable } from "./schemas/auth.schema";
+import { inviteCodesTable } from "./schemas/invite.schema";
 import { usersTable } from "./schemas/user.schema";
+import {
+	sessionParticipantsTable,
+	sessionVideosTable,
+	studySessionsTable
+} from "./schemas/studysession.schema";
 
 export const getDB = (db: D1Database) => {
 	return drizzle(db, {
 		schema: {
 			usersTable,
+
 			authSessionsTable,
+			oauthAccountsTable,
 			inviteCodesTable,
-			oauthAccountsTable
+
+			studySessionsTable,
+			sessionParticipantsTable,
+			sessionVideosTable
 		}
 	});
 };
