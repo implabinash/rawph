9bdd2dbcb22a82ca26dea22495e0dd05a364e12f
Abinash Sahoo <implabinash@gmail.com>
2025-10-27 16:02:00 +0530

    feat: add ws message types

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -9,3 +9,34 @@ export type User = {
 	createdAt: Date;
 	updatedAt: Date;
 };
+
+type MessageRecipient = "ss" | "sp";
+
+type MessageType =
+	// User actions
+	| "mute"
+	| "unmute"
+
+	// Session control
+	| "add_participant"
+	| "remove_participant"
+	| "approve_participant"
+	| "reject_participant"
+
+	// Content
+	| "remove_video"
+	| "add_video";
+
+type WSMessage<T> = {
+	type: MessageType;
+	data: T;
+	for: MessageRecipient;
+};
+
+type ParticipantData = {
+	userId: string;
+	name: string;
+	image: string;
+};
+
+export type NewParticipantMessage = WSMessage<ParticipantData>;
