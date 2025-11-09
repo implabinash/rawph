d2b9e99489514d83ad37c4c695fa2c77cf61efed
Abinash Sahoo <implabinash@gmail.com>
2025-11-09 19:38:40 +0530

    fix

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,13 @@
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	return {
-		user: locals.user
-			? {
-					id: locals.user.id,
-					email: locals.user.email,
-					name: locals.user.name
-				}
-			: null
-	};
+	const user = locals.user
+		? {
+				id: locals.user.id,
+				email: locals.user.email,
+				name: locals.user.name
+			}
+		: null;
+
+	return { user };
 };
